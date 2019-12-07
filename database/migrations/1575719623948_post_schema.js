c9d5ea59cadc818385e3149c7e329ef65f90d7b0
georgemmp <machadogeorge.p@gmail.com>
2019-12-07 09:08:39 -0300

    All configuration to create post schema

---
+++ database/migrations/1575719623948_post_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table
+        .integer('userId')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.text('body').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
